a016a5038ba5d7e6f67b96b0fa51c1e3dace0a4e
frankwmm <frankwilsonknow@gmail.com>
2021-07-16 19:51:22 -0500

    WGC up M/I

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.static(__dirname + "/dist/lp2-avance-matricula"));
 
 // Send all requests to index.html
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/lp2-avance-matricula/index.html"));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 // default Heroku port
